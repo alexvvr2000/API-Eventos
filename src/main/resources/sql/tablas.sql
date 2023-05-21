8c5559164fe263d88e934ef976ba6ebeb1115b76
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-21 15:32:28 -0600

    ahora tablas manejan por cantidad de asientos disponibles y precio unitario

--- src/main/resources/sql/tablas.sql
+++ src/main/resources/sql/tablas.sql
@@ -9,12 +9,13 @@ create table fecha (
 	idEvento integer references evento(idEvento) on delete cascade,
 	lugar varchar(30),
 	fecha timestamp,
-	filas jsonb,
+	asientosDisponibles integer default 0,
+	precioAsiento decimal,
 	cancelado boolean default false
 );
 insert into evento (descripcion)
 values ('Juan Gabriel en concierto');
-insert into fecha (idEvento, lugar, fecha, filas)
-values(1, 'Arena Mexico', NOW(), '{"A" : {"1": true, "2": true, "precio": 9000},"B" : {"1": false, "2": false, "precio": 1000}}');
-insert into fecha (idEvento, lugar, fecha, filas)
-values(1, 'Domo Care', NOW(), '{"A" : {"1": true, "2": true, "precio": 10000},"B" : {"1": true, "2": true, "precio": 2000}}');
+insert into fecha (idEvento, lugar, fecha, asientosDisponibles, precioAsiento)
+values(1, 'Arena Mexico', NOW(), 500, 2000);
+insert into fecha (idEvento, lugar, fecha, asientosDisponibles, precioAsiento)
+values(1, 'Domo Care', NOW(), 100, 5000);
