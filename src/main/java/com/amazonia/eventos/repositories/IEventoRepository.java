e437d80a6db606091571211c84b9fcee57a72040
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-21 16:04:54 -0600

    agregada anotacion de repositorio

--- src/main/java/com/amazonia/eventos/repositories/IEventoRepository.java
+++ src/main/java/com/amazonia/eventos/repositories/IEventoRepository.java
@@ -2,6 +2,8 @@ package com.amazonia.eventos.repositories;
 
 import com.amazonia.eventos.models.Evento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IEventoRepository extends JpaRepository<Evento, Integer> {
 }
