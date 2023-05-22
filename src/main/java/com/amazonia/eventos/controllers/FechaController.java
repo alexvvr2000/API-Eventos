fef2f88d0c82616c72cd60c55ab1600c6a022a10
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-22 10:13:23 -0600

    metodo put de actualizar datos

--- src/main/java/com/amazonia/eventos/controllers/FechaController.java
+++ src/main/java/com/amazonia/eventos/controllers/FechaController.java
@@ -4,11 +4,10 @@ import com.amazonia.eventos.models.Fecha;
 import com.amazonia.eventos.services.FechaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/amazonia")
@@ -22,5 +21,16 @@ public class FechaController {
                 HttpStatus.NOT_FOUND, "ID no existe en base"
         );
         return fechaEnBase;
+    }
+    @PutMapping("/fecha/{idFecha}")
+    public Fecha modificarFecha(
+            @PathVariable(name = "idFecha") Integer idFecha,
+            @RequestBody Map<String, Object> datosNuevos
+    ) {
+        Fecha fechaNueva = fechaService.actualizarFecha(idFecha, datosNuevos);
+        if(fechaNueva == null) throw new ResponseStatusException(
+                HttpStatus.NOT_ACCEPTABLE, "Campos no son parte de fecha"
+        );
+        return fechaNueva;
     }
 }
