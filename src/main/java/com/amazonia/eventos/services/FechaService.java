26ddd934f8f15cc4f9b93dea2ab898f892733bdf
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-22 09:36:15 -0600

    creado metodo para obtener por id de fecha

--- src/main/java/com/amazonia/eventos/services/FechaService.java
+++ src/main/java/com/amazonia/eventos/services/FechaService.java
@@ -1,11 +1,18 @@
 package com.amazonia.eventos.services;
 
+import com.amazonia.eventos.models.Fecha;
 import com.amazonia.eventos.repositories.IFechaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class FechaService {
     @Autowired
     private IFechaRepository iFechaRepository;
+    public Fecha obtenerFechaEvento(Integer idFecha) {
+        Optional<Fecha> fechaEnBase = iFechaRepository.findById(idFecha);
+        return fechaEnBase.orElse(null);
+    }
 }
