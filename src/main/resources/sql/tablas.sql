d35fd445f50ca6fcd3371d65ac18718a0df1d50c
Alejandro Valenzuela <alejandrovalenzuela051@gmail.com>
2023-05-22 09:11:30 -0600

    nuevos datos de prueba con las tablas de ORM

--- src/main/resources/sql/tablas.sql
+++ src/main/resources/sql/tablas.sql
@@ -1,21 +1,8 @@
-drop table if exists evento cascade;
-drop table if exists fecha cascade;
-create table evento (
-	idEvento serial primary key,
-	descripcion varchar(50)
-);
-create table fecha (
-	idFecha serial primary key,
-	idEvento integer references evento(idEvento) on delete cascade,
-	lugar varchar(30),
-	fecha timestamp,
-	asientosDisponibles integer default 0,
-	precioAsiento decimal,
-	cancelado boolean default false
-);
-insert into evento (descripcion)
+delete from public."Evento";
+delete from public."Fecha";
+insert into public."Evento" (descripcion)
 values ('Juan Gabriel en concierto');
-insert into fecha (idEvento, lugar, fecha, asientosDisponibles, precioAsiento)
+insert into public."Fecha" (idEvento, ubicacion, diaHora, asientosDisponibles, precioAsiento)
 values(1, 'Arena Mexico', NOW(), 500, 2000);
-insert into fecha (idEvento, lugar, fecha, asientosDisponibles, precioAsiento)
+insert into "Fecha" (idEvento, ubicacion, diaHora, asientosDisponibles, precioAsiento)
 values(1, 'Domo Care', NOW(), 100, 5000);
